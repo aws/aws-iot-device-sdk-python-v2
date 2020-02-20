004294336525a463853fe84dfb372abc3b90b742
Michael Graeb <graebm@amazon.com>
2020-02-20 15:40:28 -0800

    bump version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 
 setup(
     name='awsiotsdk',
-    version='1.0.4',
+    version='1.0.5',
     description='AWS IoT SDK based on the AWS Common Runtime',
     author='AWS SDK Common Runtime Team',
     url='https://github.com/aws/aws-iot-device-sdk-python-v2',
