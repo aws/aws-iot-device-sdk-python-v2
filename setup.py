db2f13d520cbc1d9a59a37f0ee6a8bd2675e90ab
Justin Boswell <boswej@amazon.com>
2020-05-12 16:14:31 -0700

    set the default version to 1.0.0-dev

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 
 setup(
     name='awsiotsdk',
-    version='1.0.7',
+    version='1.0.0-dev',
     description='AWS IoT SDK based on the AWS Common Runtime',
     author='AWS SDK Common Runtime Team',
     url='https://github.com/aws/aws-iot-device-sdk-python-v2',
