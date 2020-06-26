db8fcc58cfcc29d6cf63f99b93663f88e2c99086
Bret Ambrose <bretambrose@gmail.com>
2020-06-26 12:46:53 -0700

    Rework license block

--- utils/parse_cert_set_result.py
+++ utils/parse_cert_set_result.py
@@ -1,3 +1,6 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0.
+
 """
 
 A simply utility script to parse out certificates and keys from some IoT operations.
