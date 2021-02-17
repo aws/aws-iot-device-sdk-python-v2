e439414d7806128cf9a9248fe34571407f86ff10
rpcme <rich@richelberger.com>
2021-02-16 20:23:58 -0500

    add in classifier from aws-crt-python, change license attribute to match classifier

--- setup.py
+++ setup.py
@@ -8,11 +8,16 @@ from setuptools import setup, find_packages
 setup(
     name='awsiotsdk',
     version='1.0.0-dev',
-    license='Apache-2.0',
+    license='Apache Software License',
     description='AWS IoT SDK based on the AWS Common Runtime',
     author='AWS SDK Common Runtime Team',
     url='https://github.com/aws/aws-iot-device-sdk-python-v2',
     packages=find_packages(include=['awsiot*']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
     install_requires=[
         'awscrt==0.9.15',
     ],
