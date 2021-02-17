82696db42dfa620fa506f7c0a074d53d83a1c37b
Richard Elberger <elberger@amazon.com>
2021-02-16 20:05:59 -0500

    define the license so 'pip3 show' shows the license (#153)

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ from setuptools import setup, find_packages
 setup(
     name='awsiotsdk',
     version='1.0.0-dev',
+    license='Apache-2.0',
     description='AWS IoT SDK based on the AWS Common Runtime',
     author='AWS SDK Common Runtime Team',
     url='https://github.com/aws/aws-iot-device-sdk-python-v2',
