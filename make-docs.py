0a1ae9340056f25c788181730adad5f1eb21cf3f
Michael Graeb <graebm@amazon.com>
2021-02-24 11:22:31 -0800

    make-docs.py nukes old docs/ folder

--- make-docs.py
+++ make-docs.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 
+import shutil
 import subprocess
+
 try:
     import sphinx
 except:
     exit('sphinx must be installed via pip')
 
+# Delete existing docs/, so that old no-longer-used files don't sit around forever
+shutil.rmtree('docs', ignore_errors=True)
+
+# Run sphinx
 subprocess.check_call(['sphinx-build', 'docsrc', 'docs'])
+
+# Create empty .nojekyll file, so that Github Pages shows plain old HTML
+with open('docs/.nojekyll', 'w'):
+    pass
