e35c2add17bee4cae44295d902d5e0552f971903
Dengke Tang <dengket@amazon.com>
2021-05-14 11:19:04 -0700

    remove support of 305 as awscrt did

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         "Operating System :: OS Independent",
     ],
     install_requires=[
-        'awscrt==0.11.17',
+        'awscrt==0.11.19',
     ],
-    python_requires='>=3.5',
+    python_requires='>=3.6',
 )
