8fb1c4c55f5d517404b33e41e2ef4f7ae61ea272
Michael Graeb <graebm@amazon.com>
2022-01-26 13:36:05 -0800

    trivial test tweaks - use timeouts when waiting on futures so tests don't hang - use `self.assertEquals(x, y)` instead of `assert x == y`

--- test/test_ggv2.py
+++ test/test_ggv2.py
@@ -7,6 +7,8 @@ from awsiot.greengrasscoreipc.client import SubscribeToTopicStreamHandler
 from awsiot.greengrasscoreipc.model import CreateLocalDeploymentResponse, SubscribeToTopicResponse, \
     SubscriptionResponseMessage, BinaryMessage
 
+TIMEOUT = 10.0 # seconds
+
 
 class GGV2Test(TestCase):
 
@@ -29,7 +31,7 @@ class GGV2Test(TestCase):
         self._mock_operation(mock_deployment_op, CreateLocalDeploymentResponse(deployment_id="deployment"))
         mock_client.new_create_local_deployment.return_value = mock_deployment_op
         resp = c.create_local_deployment()
-        assert resp.deployment_id == "deployment"
+        self.assertEqual("deployment", resp.deployment_id)
 
         # Verify subscription works and callback is called on the executor thread
         self._mock_operation(mock_subscribe_op, SubscribeToTopicResponse(topic_name="abc"))
@@ -42,13 +44,13 @@ class GGV2Test(TestCase):
             subscription_fut.set_result(r)
             thread_id_fut.set_result(threading.get_ident())
         resp, op = c.subscribe_to_topic(topic="abc", on_stream_event=on_stream_event)
-        assert resp.topic_name == "abc"
+        self.assertEqual("abc", resp.topic_name)
 
         sub_handler = mock_client.new_subscribe_to_topic.call_args[0][0]
         sub_handler.on_stream_event(SubscriptionResponseMessage(binary_message=BinaryMessage(message="xyz")))
 
-        assert subscription_fut.result().binary_message.message == "xyz".encode("utf-8")
-        assert threading.get_ident() != thread_id_fut.result()
+        self.assertEqual("xyz".encode("utf-8"), subscription_fut.result(TIMEOUT).binary_message.message)
+        self.assertNotEqual(threading.get_ident(), thread_id_fut.result(TIMEOUT))
 
         # Verify that when using the stream_handler option, the callback is run in the executor
 
@@ -59,13 +61,13 @@ class GGV2Test(TestCase):
             def on_stream_event(self, event):
                 on_stream_event(event)
         resp, op = c.subscribe_to_topic(topic="abc", stream_handler=handler())
-        assert resp.topic_name == "abc"
+        self.assertEqual("abc", resp.topic_name)
 
         sub_handler = mock_client.new_subscribe_to_topic.call_args[0][0]
         sub_handler.on_stream_event(SubscriptionResponseMessage(binary_message=BinaryMessage(message="xyz")))
 
-        assert subscription_fut.result().binary_message.message == "xyz".encode("utf-8")
-        assert threading.get_ident() != thread_id_fut.result()
+        self.assertEqual("xyz".encode("utf-8"), subscription_fut.result(TIMEOUT).binary_message.message)
+        self.assertNotEqual(threading.get_ident(), thread_id_fut.result(TIMEOUT))
 
         # Remove executor from client to verify that we are not running the callback in a different thread
         c = Client(client=mock_client, executor=None)
@@ -74,10 +76,10 @@ class GGV2Test(TestCase):
         thread_id_fut = concurrent.futures.Future()
 
         resp, op = c.subscribe_to_topic(topic="abc", on_stream_event=on_stream_event)
-        assert resp.topic_name == "abc"
+        self.assertEqual("abc", resp.topic_name)
 
         sub_handler = mock_client.new_subscribe_to_topic.call_args[0][0]
         sub_handler.on_stream_event(SubscriptionResponseMessage(binary_message=BinaryMessage(message="xyz")))
 
-        assert subscription_fut.result().binary_message.message == "xyz".encode("utf-8")
-        assert threading.get_ident() == thread_id_fut.result()
+        self.assertEqual("xyz".encode("utf-8"), subscription_fut.result(TIMEOUT).binary_message.message)
+        self.assertEqual(threading.get_ident(), thread_id_fut.result(TIMEOUT))
