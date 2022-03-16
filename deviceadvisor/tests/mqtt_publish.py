95cdf58fd1f48e3f9b482b217a2f95b040451122
Vera Xia <zhvxia@amazon.com>
2022-03-16 14:17:01 -0700

    extend timeout time

--- deviceadvisor/tests/mqtt_publish.py
+++ deviceadvisor/tests/mqtt_publish.py
@@ -16,7 +16,8 @@ if __name__ == '__main__':
         cert_filepath=DATestUtils.certificatePath,
         pri_key_filepath=DATestUtils.keyPath,
         client_id=DATestUtils.client_id,
-        clean_session = True)
+        clean_session = True,
+        ping_timeout_ms = 6000)
     connect_future = mqtt_connection.connect()
 
     # Future.result() waits until a result is available
