b7b03e75d024c9a346b1e6dc4a49f0e2b21db1f2
Steve Kim <sbstevek@amazon.com>
2022-03-24 15:32:07 -0700

    to and from test

--- test/test_shadowState.py
+++ test/test_shadowState.py
@@ -1,11 +1,10 @@
 from unittest import TestCase
-from unittest.mock import create_autospec
 
 from awsiot.iotshadow import IotShadowClient, ShadowState
 
 
 class ShadowStateToAndFromTest(TestCase):
 
-    def test_update_reboot_requested_state(self):
+    def test_to_and_from_payload_parity(self):
         expected_state = ShadowState.from_payload({"reported": {"deviceAgent": {"rebootRequired": True}}})
         assert {"reported": {"deviceAgent": {"rebootRequired": True}}} == expected_state.to_payload()
