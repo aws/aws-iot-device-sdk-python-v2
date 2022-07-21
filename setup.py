78048e1fe7e72e13eb56cefcfb8fbefd4310a116
TwistedTwigleg <ncbeard@amazon.com>
2022-07-21 16:58:49 -0400

    Update to use latest CRT (#334)
    * Update to use latest CRT
    * Fix CI documentation check

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         "Operating System :: OS Independent",
     ],
     install_requires=[
-        'awscrt==0.13.13',
+        'awscrt==0.13.14',
     ],
     python_requires='>=3.6',
 )
