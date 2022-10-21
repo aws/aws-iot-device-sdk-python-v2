3b85082220f5e663f47f5f829055651e282b488c
Noah Beard <ncbeard@amazon.com>
2022-10-21 13:45:25 -0400

    This is not java, so no maven

--- codebuild/samples/custom-auth-linux.sh
+++ codebuild/samples/custom-auth-linux.sh
@@ -10,8 +10,6 @@ ENDPOINT=$(aws secretsmanager get-secret-value --secret-id "ci/endpoint" --query
 AUTH_NAME=$(aws secretsmanager get-secret-value --secret-id "ci/CustomAuthorizer/name" --query "SecretString" | cut -f2 -d":" | sed -e 's/[\\\"\}]//g')
 AUTH_PASSWORD=$(aws secretsmanager get-secret-value --secret-id "ci/CustomAuthorizer/password" --query "SecretString" | cut -f2 -d":" | sed -e 's/[\\\"\}]//g')
 
-mvn compile
-
 echo "Custom Authorizer test"
 python3 custom_authorizer_connect.py --endpoint $ENDPOINT --custom_auth_authorizer_name $AUTH_NAME --custom_auth_password $AUTH_PASSWORD
 
