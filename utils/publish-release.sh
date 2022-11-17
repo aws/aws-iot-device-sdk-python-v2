156839749bd956168c90aa47af3fee5e0ed29757
Noah Beard <ncbeard@amazon.com>
2022-11-17 09:53:22 -0500

    Use a Python script to update the version and check type input is correct

--- utils/publish-release.sh
+++ utils/publish-release.sh
@@ -14,17 +14,6 @@ if [ "$#" != "2" ]; then
     exit 1
 fi
 
-# Increments the version up by one
-# Credit: https://stackoverflow.com/a/64390598
-increment_version() {
-  local delimiter=.
-  local array=($(echo "$1" | tr $delimiter '\n'))
-  array[$2]=$((array[$2]+1))
-  if [ $2 -lt 2 ]; then array[2]=0; fi
-  if [ $2 -lt 1 ]; then array[1]=0; fi
-  echo $(local IFS=$delimiter ; echo "${array[*]}")
-}
-
 pushd $(dirname $0) > /dev/null
 
 # Get the current version
@@ -34,15 +23,9 @@ current_version_without_v=$(echo ${current_version} | cut -f2 -dv)
 
 echo "Current release version is ${current_version_without_v}"
 
-# Validate that RELEASE_TYPE is what we expect and bump the version:
-new_version="${current_version_without_v}"
-if [ "$RELEASE_TYPE" == "bug fix (PATCH)" ]; then
-    new_version=$(increment_version ${current_version_without_v} 2 )
-elif [ "$RELEASE_TYPE" == "new feature (MINOR)" ]; then
-    new_version=$(increment_version ${current_version_without_v} 1 )
-elif [ "$RELEASE_TYPE" == "new version (MAJOR)" ]; then
-    new_version=$(increment_version ${current_version_without_v} 0 )
-else
+# Validate that RELEASE_TYPE is what we expect and bump the version
+new_version=$(python3 ./update_semantic_version.py --version "${current_version_without_v}" --type "${RELEASE_TYPE}")
+if [ "$new_version" == "0.0.0" ]; then
     echo "ERROR: Unknown release type! Exitting..."
     exit -1
 fi
