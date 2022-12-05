124ce5c706bd743bb25b37eedecbb22fc562b6a5
Noah Beard <ncbeard@amazon.com>
2022-12-05 11:11:56 -0500

    More small fixes

--- awsiot/greengrass_discovery.py
+++ awsiot/greengrass_discovery.py
@@ -19,7 +19,7 @@ class DiscoveryClient:
         tls_context: Client TLS context
         region: AWS region (not used if gg_server_name is set)
         gg_server_name: optional full server name
-        proxy_options (HttpProxyOptions): Optional proxy options. If None is provided then a proxy is not used.
+        proxy_options: Proxy options (if None is provided then a proxy is not used)
     """
     __slots__ = [
         '_bootstrap',
@@ -30,7 +30,7 @@ class DiscoveryClient:
         '_gg_server_name',
         'gg_url',
         'port',
-        "proxy_options"]
+        "_proxy_options"]
 
     def __init__(
             self,
