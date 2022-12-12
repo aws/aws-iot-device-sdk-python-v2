695dbfbe2857b530fc1eb023028bcea771f818dd
Noah Beard <ncbeard@amazon.com>
2022-12-12 16:07:46 -0500

    Add custom authorizer support to MQTT5

--- samples/mqtt5_custom_authorizer_connect.py
+++ samples/mqtt5_custom_authorizer_connect.py
@@ -18,6 +18,7 @@ cmdUtils.add_common_custom_authorizer_commands()
 cmdUtils.register_command("client_id", "<str>",
                           "Client ID to use for MQTT connection (optional, default='test-*').",
                           default="test-" + str(uuid4()))
+cmdUtils.register_command("use_websockets", "<str>", "If set, websockets will be used (optional, do not set to use direct MQTT)")
 cmdUtils.register_command("is_ci", "<str>", "If present the sample will run in CI mode (optional, default='None')")
 # Needs to be called so the command utils parse the commands
 cmdUtils.get_args()
@@ -25,6 +26,7 @@ cmdUtils.get_args()
 future_stopped = Future()
 future_connection_success = Future()
 is_ci = cmdUtils.get_command("is_ci", None) != None
+use_websockets = cmdUtils.get_command("use_websockets", None) != None
 
 # Callback for the lifecycle event Stopped
 def on_lifecycle_stopped(lifecycle_stopped_data: mqtt5.LifecycleStoppedData):
@@ -42,16 +44,29 @@ def on_lifecycle_connection_success(lifecycle_connect_success_data: mqtt5.Lifecy
 if __name__ == '__main__':
 
     # Create MQTT5 Client with a custom authorizer
-    client = mqtt5_client_builder.direct_with_custom_authorizer(
-        endpoint=cmdUtils.get_command_required(cmdUtils.m_cmd_endpoint),
-        ca_filepath=cmdUtils.get_command(cmdUtils.m_cmd_ca_file),
-        auth_username=cmdUtils.get_command(cmdUtils.m_cmd_custom_auth_username),
-        auth_authorizer_name=cmdUtils.get_command(cmdUtils.m_cmd_custom_auth_authorizer_name),
-        auth_authorizer_signature=cmdUtils.get_command(cmdUtils.m_cmd_custom_auth_authorizer_signature),
-        auth_password=cmdUtils.get_command(cmdUtils.m_cmd_custom_auth_password),
-        on_lifecycle_stopped=on_lifecycle_stopped,
-        on_lifecycle_connection_success=on_lifecycle_connection_success,
-        client_id=cmdUtils.get_command("client_id"))
+    client = None
+    if use_websockets == None:
+        client = mqtt5_client_builder.direct_with_custom_authorizer(
+            endpoint=cmdUtils.get_command_required(cmdUtils.m_cmd_endpoint),
+            ca_filepath=cmdUtils.get_command(cmdUtils.m_cmd_ca_file),
+            auth_username=cmdUtils.get_command(cmdUtils.m_cmd_custom_auth_username),
+            auth_authorizer_name=cmdUtils.get_command(cmdUtils.m_cmd_custom_auth_authorizer_name),
+            auth_authorizer_signature=cmdUtils.get_command(cmdUtils.m_cmd_custom_auth_authorizer_signature),
+            auth_password=cmdUtils.get_command(cmdUtils.m_cmd_custom_auth_password),
+            on_lifecycle_stopped=on_lifecycle_stopped,
+            on_lifecycle_connection_success=on_lifecycle_connection_success,
+            client_id=cmdUtils.get_command("client_id"))
+    else:
+        client = mqtt5_client_builder.websockets_with_custom_authorizer(
+            endpoint=cmdUtils.get_command_required(cmdUtils.m_cmd_endpoint),
+            ca_filepath=cmdUtils.get_command(cmdUtils.m_cmd_ca_file),
+            auth_username=cmdUtils.get_command(cmdUtils.m_cmd_custom_auth_username),
+            auth_authorizer_name=cmdUtils.get_command(cmdUtils.m_cmd_custom_auth_authorizer_name),
+            auth_authorizer_signature=cmdUtils.get_command(cmdUtils.m_cmd_custom_auth_authorizer_signature),
+            auth_password=cmdUtils.get_command(cmdUtils.m_cmd_custom_auth_password),
+            on_lifecycle_stopped=on_lifecycle_stopped,
+            on_lifecycle_connection_success=on_lifecycle_connection_success,
+            client_id=cmdUtils.get_command("client_id"))
 
     if is_ci == False:
         print("Connecting to {} with client ID '{}'...".format(
@@ -61,7 +76,7 @@ if __name__ == '__main__':
 
     client.start()
     future_connection_success.result(TIMEOUT)
-    print("Clint Connected")
+    print("Client Connected")
 
     print("Stopping Client")
     client.stop()
