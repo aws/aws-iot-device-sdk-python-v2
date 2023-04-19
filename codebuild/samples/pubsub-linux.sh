5978ac52f444aa1b41f7f83db201b68957cc0bd5
Noah Beard <ncbeard@amazon.com>
2023-04-18 20:15:46 -0400

    Make sure all PubSub samples know they are running in CI

--- codebuild/samples/pubsub-linux.sh
+++ codebuild/samples/pubsub-linux.sh
@@ -10,6 +10,6 @@ pushd $CODEBUILD_SRC_DIR/samples/
 ENDPOINT=$(aws secretsmanager get-secret-value --secret-id "ci/endpoint" --query "SecretString" | cut -f2 -d":" | sed -e 's/[\\\"\}]//g')
 
 echo "PubSub test"
-python3 pubsub.py --endpoint $ENDPOINT --key /tmp/privatekey.pem --cert /tmp/certificate.pem
+python3 pubsub.py --endpoint $ENDPOINT --key /tmp/privatekey.pem --cert /tmp/certificate.pem --is_ci "true"
 
 popd
