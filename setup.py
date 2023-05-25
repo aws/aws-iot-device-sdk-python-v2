532be2f65f0046b682e2baba98dcbdfd9bcfbfa0
Steve Kim <sbstevek@amazon.com>
2023-05-25 10:42:55 -0700

    update to latest version of crt

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         "Operating System :: OS Independent",
     ],
     install_requires=[
-        'awscrt==0.16.17',
+        'awscrt==0.16.18',
     ],
     python_requires='>=3.7',
 )
