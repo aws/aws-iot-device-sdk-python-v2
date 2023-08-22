d643c59591e28a80e19c5a9db4c812e6e49f2e4e
Talby <yastalby@b0be8380182d.ant.amazon.com>
2023-08-22 13:08:09 +0200

    fix

--- awsiot/mqtt5_client_builder.py
+++ awsiot/mqtt5_client_builder.py
@@ -113,7 +113,8 @@ Optional Keyword Arguments (omit, or set `None` to get default value):
         The function should take the following arguments and return nothing:
 
         *   `publish_received_data` (:class:`awscrt.mqtt5.PublishReceivedData`): Dataclass containing the following:
-            *`publish_packet`: (:class:`awscrt.mqtt5.PublishPacket`): Publish Packet received from the server.
+
+            *`publish_packet`: (:class:`awscrt.mqtt5.PublishPacket`): Data model of an `MQTT5 PUBLISH <https://docs.oasis-open.org/mqtt/mqtt/v5.0/os/mqtt-v5.0-os.html#_Toc3901100>` _ packet.
 
     **on_lifecycle_stopped** (`Callable`): Callback invoked for Lifecycle Event Stopped.
         The function should take the following arguments and return nothing:
