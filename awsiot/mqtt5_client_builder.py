7709bbb550e78fbc29b146923570011d153dacc3
Steve Kim <sbstevek@amazon.com>
2024-01-24 09:55:41 -0800

    Add default ping timeout

--- awsiot/mqtt5_client_builder.py
+++ awsiot/mqtt5_client_builder.py
@@ -185,7 +185,8 @@ import urllib.parse
 
 DEFAULT_WEBSOCKET_MQTT_PORT = 443
 DEFAULT_DIRECT_MQTT_PORT = 8883
-DEFAULT_KEEP_ALIVE = 1200
+DEFAULT_KEEP_ALIVE_SEC = 1200
+DEFAULT_PING_TIMEOUT_MS = 29000
 
 
 def _check_required_kwargs(**kwargs):
@@ -287,7 +288,7 @@ def _builder(
         client_options.min_connected_time_to_reset_reconnect_delay_ms = _get(
             kwargs, 'min_connected_time_to_reset_reconnect_delay_ms')
     if client_options.ping_timeout_ms is None:
-        client_options.ping_timeout_ms = _get(kwargs, 'ping_timeout_ms')
+        client_options.ping_timeout_ms = _get(kwargs, 'ping_timeout_ms', DEFAULT_PING_TIMEOUT_MS)
     if client_options.connack_timeout_ms is None:
         client_options.connack_timeout_ms = _get(kwargs, 'connack_timeout_ms')
     if client_options.ack_timeout_sec is None:
@@ -302,7 +303,7 @@ def _builder(
         client_options.connect_options.client_id = _get(kwargs, 'client_id')
     if client_options.connect_options.keep_alive_interval_sec is None:
         client_options.connect_options.keep_alive_interval_sec = _get(
-            kwargs, 'keep_alive_interval_sec', DEFAULT_KEEP_ALIVE)
+            kwargs, 'keep_alive_interval_sec', DEFAULT_KEEP_ALIVE_SEC)
     client_options.connect_options.username = username
     if client_options.connect_options.password is None:
         client_options.connect_options.password = _get(kwargs, 'password')
