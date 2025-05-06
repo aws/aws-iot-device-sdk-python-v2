defefac39305f6edc941ad05f5962931b0c62a44
Bret Ambrose <bambrose@amazon.com>
2025-05-06 15:30:19 -0700

    Jobs processing tests

--- test/test_jobs.py
+++ test/test_jobs.py
@@ -1,17 +1,20 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0.
 import pdb
+import threading
 
 from awscrt import io, mqtt, mqtt5, mqtt_request_response
 import awsiot
 from awsiot import iotjobs
 
 import boto3
+from botocore.exceptions import ClientError
 from concurrent.futures import Future
 import os
 import time
 import unittest
 import uuid
+
 
 TIMEOUT = 30.0
 
@@ -62,10 +65,15 @@ class JobsTestCallbacks():
 class TestContext():
     def __init__(self):
         self.region = _get_env_variable("AWS_TEST_MQTT5_IOT_CORE_REGION")
+        self.iot_client = boto3.client('iot', self.region)
         self.thing_name = None
         self.thing_group_name = None
         self.thing_group_arn = None
         self.job_id = None
+        self.job_executions_changed_events = []
+        self.next_job_execution_changed_events = []
+        self.lock = threading.Lock()
+        self.signal = threading.Condition(lock=self.lock)
 
 class JobsServiceTest(unittest.TestCase):
 
@@ -181,8 +189,201 @@ class JobsServiceTest(unittest.TestCase):
         self._shutdown_protocol_client311(protocol_client)
 
     def _tear_down(self, test_context):
-        pass
+        if test_context.iot_client is None:
+            return
 
+        if test_context.job_id is not None:
+            done = False
+            while not done:
+                try:
+                    test_context.iot_client.delete_job(jobId=test_context.job_id, force=True)
+                    done=True
+                except ClientError as ce:
+                    exception_type = ce.response['Error']['Code']
+                    if exception_type == 'ThrottlingException' or exception_type == 'LimitExceededException':
+                        time.sleep(10)
+                    elif exception_type == 'ResourceNotFoundException':
+                        done = True
+
+
+        if test_context.thing_name is not None:
+            test_context.iot_client.delete_thing(thingName=test_context.thing_name)
+
+        if test_context.thing_group_name is not None:
+            test_context.iot_client.delete_thing_group(thingGroupName=test_context.thing_group_name)
+
+    def _setup(self, test_context):
+        tgn = "tgn-" + uuid.uuid4().hex
+
+        create_tg_response = test_context.iot_client.create_thing_group(thingGroupName=tgn)
+
+        test_context.thing_group_name = tgn
+        test_context.thing_group_arn = create_tg_response['thingGroupArn']
+
+        thing_name = "thing-" + uuid.uuid4().hex
+
+        test_context.iot_client.create_thing(thingName=thing_name)
+        test_context.thing_name = thing_name
+
+        time.sleep(1)
+
+        job_id = "job-" + uuid.uuid4().hex
+        job_document = '{"test":"do-something"}'
+
+        test_context.iot_client.create_job(
+            jobId=job_id,
+            document=job_document,
+            targets=[test_context.thing_group_arn],
+            targetSelection='CONTINUOUS')
+
+        test_context.job_id = job_id
+
+    def _create_job_executions_changed_stream(self, test_context, jobs_client):
+        subscribed = Future()
+
+        def on_incoming_publish_event(event):
+            with test_context.signal:
+                test_context.job_executions_changed_events.append(event)
+                test_context.signal.notify_all()
+
+        def on_subscription_event(event):
+            subscribed.set_result(event)
+
+        stream_options = awsiot.ServiceStreamOptions(
+            incoming_event_listener=on_incoming_publish_event,
+            subscription_status_listener=on_subscription_event
+        )
+
+        stream = jobs_client.create_job_executions_changed_stream(
+            iotjobs.JobExecutionsChangedSubscriptionRequest(thing_name=test_context.thing_name),
+            stream_options)
+        stream.open()
+
+        subscription_event = subscribed.result(TIMEOUT)
+        self.assertEqual(mqtt_request_response.SubscriptionStatusEventType.SUBSCRIPTION_ESTABLISHED,
+                         subscription_event.type)
+
+        return stream
+
+    def _create_next_job_execution_changed_stream(self, test_context, jobs_client):
+        subscribed = Future()
+
+        def on_incoming_publish_event(event):
+            with test_context.signal:
+                test_context.next_job_execution_changed_events.append(event)
+                test_context.signal.notify_all()
+
+        def on_subscription_event(event):
+            subscribed.set_result(event)
+
+        stream_options = awsiot.ServiceStreamOptions(
+            incoming_event_listener=on_incoming_publish_event,
+            subscription_status_listener=on_subscription_event
+        )
+
+        stream = jobs_client.create_next_job_execution_changed_stream(
+            iotjobs.NextJobExecutionChangedSubscriptionRequest(thing_name=test_context.thing_name),
+            stream_options)
+        stream.open()
+
+        subscription_event = subscribed.result(TIMEOUT)
+        self.assertEqual(mqtt_request_response.SubscriptionStatusEventType.SUBSCRIPTION_ESTABLISHED,
+                         subscription_event.type)
+
+        return stream
+
+    def _wait_for_initial_stream_events(self, test_context):
+        with test_context.signal:
+            while len(test_context.next_job_execution_changed_events) == 0:
+                test_context.signal.wait()
+
+            next_job_execution_changed_event = test_context.next_job_execution_changed_events[0]
+            self.assertEqual(test_context.job_id, next_job_execution_changed_event.execution.job_id)
+            self.assertEqual(iotjobs.JobStatus.QUEUED, next_job_execution_changed_event.execution.status)
+
+            while len(test_context.job_executions_changed_events) == 0:
+                test_context.signal.wait()
+
+            job_executions_changed_event = test_context.job_executions_changed_events[0]
+            queued_jobs = job_executions_changed_event.jobs[iotjobs.JobStatus.QUEUED]
+            self.assertTrue(len(queued_jobs) > 0)
+            self.assertEqual(test_context.job_id, queued_jobs[0].job_id)
+
+    def _wait_for_final_stream_events(self, test_context):
+        with test_context.signal:
+            while len(test_context.next_job_execution_changed_events) < 2:
+                test_context.signal.wait()
+
+            final_next_job_execution_changed_event = test_context.next_job_execution_changed_events[1]
+            self.assertIsNotNone(final_next_job_execution_changed_event.timestamp)
+            self.assertIsNone(final_next_job_execution_changed_event.execution)
+
+            while len(test_context.job_executions_changed_events) < 2:
+                test_context.signal.wait()
+
+            final_job_executions_changed_event = test_context.job_executions_changed_events[1]
+            self.assertTrue(final_job_executions_changed_event.jobs == None or
+                            len(final_job_executions_changed_event.jobs) == 0)
+
+    def _verify_nothing_in_progress(self, test_context, jobs_client):
+        get_pending_response = jobs_client.get_pending_job_executions(
+            iotjobs.GetPendingJobExecutionsRequest(
+                thing_name=test_context.thing_name
+            )
+        ).result(TIMEOUT)
+
+        self.assertEqual(0, len(get_pending_response.queued_jobs))
+        self.assertEqual(0, len(get_pending_response.in_progress_jobs))
+
+    def _do_job_processing_test(self, jobs_client):
+        test_context = TestContext()
+        try:
+            self._setup(test_context)
+            job_executions_changed_stream = self._create_job_executions_changed_stream(test_context, jobs_client)
+            next_job_execution_changed_stream = self._create_next_job_execution_changed_stream(test_context, jobs_client)
+
+            self._verify_nothing_in_progress(test_context, jobs_client)
+
+            test_context.iot_client.add_thing_to_thing_group(
+                thingName=test_context.thing_name,
+                thingGroupName=test_context.thing_group_name
+            )
+
+            self._wait_for_initial_stream_events(test_context)
+
+            # start the job
+            start_next_response = jobs_client.start_next_pending_job_execution(
+                iotjobs.StartNextPendingJobExecutionRequest(thing_name=test_context.thing_name)
+            ).result(TIMEOUT)
+
+            self.assertEqual(test_context.job_id, start_next_response.execution.job_id)
+
+            # pretend to do the job
+            time.sleep(1)
+
+            # verify in progress
+            describe_job_response = jobs_client.describe_job_execution(
+                iotjobs.DescribeJobExecutionRequest(
+                    job_id=test_context.job_id,
+                    thing_name=test_context.thing_name
+                )
+            ).result(TIMEOUT)
+
+            self.assertEqual(test_context.job_id, describe_job_response.execution.job_id)
+            self.assertEqual(iotjobs.JobStatus.IN_PROGRESS, describe_job_response.execution.status)
+
+            # complete the job
+            jobs_client.update_job_execution(iotjobs.UpdateJobExecutionRequest(
+                job_id=test_context.job_id,
+                thing_name=test_context.thing_name,
+                status=iotjobs.JobStatus.SUCCEEDED
+            )).result(TIMEOUT)
+
+            self._wait_for_final_stream_events(test_context)
+            self._verify_nothing_in_progress(test_context, jobs_client)
+
+        finally:
+            self._tear_down(test_context)
 
     # ==============================================================
     #             CREATION SUCCESS TEST CASES
@@ -202,7 +403,11 @@ class JobsServiceTest(unittest.TestCase):
     # ==============================================================
     #             REQUEST RESPONSE OPERATION TEST CASES
     # ==============================================================
+    def test_job_processing5(self):
+        self._do_jobs_operation_test5(lambda jobs_client: self._do_job_processing_test(jobs_client))
 
+    def test_job_processing311(self):
+        self._do_jobs_operation_test311(lambda jobs_client: self._do_job_processing_test(jobs_client))
 
 if __name__ == 'main':
     unittest.main()
