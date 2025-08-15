b20d409f2b301b156570682a35c07fd8f48d9692
Steve Kim <sbstevek@amazon.com>
2025-08-15 09:43:45 -0700

    fix remove_me

--- awsiot/remove_me.py
+++ awsiot/remove_me.py
@@ -5,12 +5,8 @@
 Cross-platform library for `awscrt`.
 """
 from typing import TYPE_CHECKING
-import _awscrt
 
 __all__ = [
-    "get_cpu_group_count",
-    "get_cpu_count_for_group",
-    "join_all_native_threads",
     "deprecated",
 ]
 
@@ -43,36 +39,3 @@ else:
                 # older typing_extensions doesn't support the 'since' kwarg
                 pass
         return _deprecated_impl(msg)
-
-
-def get_cpu_group_count() -> int:
-    """
-    Returns number of processor groups on the system.
-
-    Useful for working with non-uniform memory access (NUMA) nodes.
-    """
-    return _awscrt.get_cpu_group_count()
-
-
-def get_cpu_count_for_group(group_idx: int) -> int:
-    """
-    Returns number of processors in a given group.
-    """
-    return _awscrt.get_cpu_count_for_group(group_idx)
-
-
-def join_all_native_threads(*, timeout_sec: float = -1.0) -> bool:
-    """
-    Waits for all native threads to complete their join call.
-
-    This can only be safely called from the main thread.
-    This call may be required for native memory usage to reach zero.
-
-    Args:
-        timeout_sec (float): Number of seconds to wait before a timeout exception is raised.
-            By default the wait is unbounded.
-
-    Returns:
-        bool: Returns whether threads could be joined before the timeout.
-    """
-    return _awscrt.thread_join_all_managed(timeout_sec)
