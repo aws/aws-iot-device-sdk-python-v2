7561ef66d95f2d41dc292aae1500f9c51b210604
Steve Kim <sbstevek@amazon.com>
2025-08-18 13:26:19 -0700

    update custom auth sample

--- samples/mqtt5_custom_authorizer_connect.py
+++ samples/mqtt5_custom_authorizer_connect.py
@@ -1,17 +1,62 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0.
 
+# --------------------------------- ARGUMENT PARSING -----------------------------------------
+import argparse, uuid
+
+def parse_sample_input():
+    parser = argparse.ArgumentParser(
+        description="MQTT5 pub/sub sample (mTLS).",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument("--endpoint", required=True, dest="input_endpoint", help="IoT endpoint hostname")
+    parser.add_argument("--ca", dest="input_ca", help="Path to optional CA bundle (PEM)")
+
+    parser.add_argument("--custom_auth_username", dest="input_custom_auth_username"
+                        required="", help="The name to send when connecting through the custom authorizer (optional)")
+    parser.add_argument("--custom_auth_authorizer_name", dest="input_custom_auth_authorizer_name"
+                        help="The name of the custom authorizer to connect to (optional but required for everything but custom domains)")
+    parser.add_argument("--custom_auth_authorizer_signature", dest="input_custom_auth_authorizer_signature",
+                        help="The signature to send when connecting through a custom authorizer (optional)")
+    parser.add_argument("--custom_auth_password", dest="input_custom_auth_password",
+                        help="The password to send when connecting through a custom authorizer (optional)")
+    parser.add_argument("--custom_auth_token_key_name", dest="input_custom_auth_token_key_name",
+                        help="Key used to extract the custom authorizer token (optional)")
+    parser.add_argument("--custom_auth_token_value", dest="input_custom_auth_token_value",
+                        help="The opaque token value for the custom authorizer (optional)")
+    
+    parser.add_argument("--use_websockets", dest="input_use_websockets",
+                        action="store_const", const=True, default=None,
+                        help="Use WebSockets instead of direct TLS")
+    parser.add_argument("--use_websockets", dest="input_use_websockets")
+
+    # Misc
+    parser.add_argument("--client-id", dest="input_clientId",
+                        default=f"mqtt5-sample-{uuid.uuid4().hex[:8]}", help="Client ID")
+    
+    args = parser.parse_args()
+
+    # Validate custom-authorizer inputs:
+    # Either provide --custom-auth-username, OR provide signature + token key + token value.
+    has_username = bool(args.input_custom_auth_username)
+    has_token_triplet = all([
+        args.input_custom_authorizer_signature,
+        args.input_custom_authorizer_token_key_name,
+        args.input_custom_authorizer_token_value,
+    ])
+
+    return args
+
+cmdData = parse_sample_input()
+
+# --------------------------------- ARGUMENT PARSING END -----------------------------------------
+
 from awsiot import mqtt5_client_builder
 from awscrt import mqtt5
 from concurrent.futures import Future
-from utils.command_line_utils import CommandLineUtils
 
 TIMEOUT = 100
-
-# cmdData is the arguments/input from the command line placed into a single struct for
-# use in this sample. This handles all of the command line parsing, validating, etc.
-# See the Utils/CommandLineUtils for more information.
-cmdData = CommandLineUtils.parse_sample_input_mqtt5_custom_authorizer_connect()
 
 future_stopped = Future()
 future_connection_success = Future()
